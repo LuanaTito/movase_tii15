fe419de162385fe10ed533da647d29bb2f7c6774
LuanaTito <luana.dmelo2@senacsp.edu.br>
2026-08-06 22:15:52 -0300

    Atualização 06/08

---
+++ js/interacoes.js
@@ -0,0 +1,45 @@
+// Menu hamburger
+function clickMenu(){
+    let itens = document.getElementById("itens");
+    if (itens.style.display ==="block" || itens.style.display===""){
+        itens.style.display = "none";
+    }else{
+        itens.style.display = "block";
+    }
+    }
+
+//código do Jean
+// let itens = document.getElementById("itens");
+// let iconMenu = document.getElementById("iconMenu");
+ 
+// itens.style.display = "none";
+// iconMenu.addEventListener("click", function(event) {
+//     event.stopPropagation();
+//     if (itens.style.display === "none") {
+//         itens.style.display = "block";
+//     } else {
+//         itens.style.display = "none";
+//     }
+// });
+ 
+// document.addEventListener("click", function() {
+//     if (itens.style.display === "block") {
+//         itens.style.display = "none";
+//     }
+// });
+ 
+// itens.addEventListener("click", function(event) {
+//     event.stopPropagation();
+// });
+
+
+// Carrossel
+$(document).ready(function(){
+    $('#destaque').slick({
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:3000,
+        arrows:false
+    });
+});
+ 
