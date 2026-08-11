a275140c69aa8baebd7cf06a9d6ddfcc911bc6fc
LuanaTito <luana.dmelo2@senacsp.edu.br>
2026-08-10 21:36:27 -0300

    atualizações 10/08

--- js/interacoes.js
+++ js/interacoes.js
@@ -43,3 +43,15 @@ $(document).ready(function(){
     });
 });
  
+//modal
+
+var modalBtn = document.getElementById("modal");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
